e10270309d3e3ca426324b53689e6d651ebf19d7
Romain Kuzniak <romain.kuzniak@simple-it.fr>
2013-07-26 09:04:00 +0200

    Add part

--- src/SimpleIT/ClaireAppBundle/Controller/AssociatedContent/Component/TagController.php
+++ src/SimpleIT/ClaireAppBundle/Controller/AssociatedContent/Component/TagController.php
@@ -3,12 +3,32 @@
 
 namespace SimpleIT\ClaireAppBundle\Controller\AssociatedContent\Component;
 
+use SimpleIT\AppBundle\Controller\AppController;
+
 /**
  * Class TagController
- * 
+ *
  * @author Romain Kuzniak <romain.kuzniak@simple-it.fr>
  */
-class TagController
+class TagController extends AppController
 {
 
+    /**
+     * View a tag
+     *
+     * @param int | string $tagIdentifier Tag id | slug
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function viewAction($tagIdentifier)
+    {
+        $tag = $this->get('simple_it.claire.associated_content.tag')->get(
+            $tagIdentifier
+        );
+
+        return $this->render(
+            'SimpleITClaireAppBundle:AssociatedContent/Tag/Component:view.html.twig',
+            array('tag' => $tag)
+        );
+    }
 }
