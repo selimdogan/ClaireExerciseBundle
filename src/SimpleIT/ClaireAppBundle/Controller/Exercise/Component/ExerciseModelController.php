96db2a979d98893d6a31a12513dcec0f075454f5
Romain Kuzniak <romain.kuzniak@simple-it.fr>
2013-08-22 15:11:17 +0200

    Add exercise model

---
+++ src/SimpleIT/ClaireAppBundle/Controller/Exercise/Component/ExerciseModelController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SimpleIT\ClaireAppBundle\Controller\Exercise\Component;
+
+use SimpleIT\AppBundle\Controller\AppController;
+
+/**
+ * Class ExerciseModelController
+ *
+ * @author Romain Kuzniak <romain.kuzniak@simple-it.fr>
+ */
+class ExerciseModelController extends AppController
+{
+    /**
+     * View an exercise model
+     *
+     * @param int $exerciseModelId Exercise Model Id
+     */
+    public function viewAction($exerciseModelId)
+    {
+        $exerciseModel = $this->get('simple_it.claire.exercise.exercise_model')->get($exerciseModelId);
+
+        $this->render(
+            'SimpleITClaireAppBundle:Exercise/ExerciseModel/Component:view.html.twig',
+            array('exerciseModel' => $exerciseModel)
+        );
+    }
+}
