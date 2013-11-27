05c4ecb5131497bf6c9846f4c043eadca09dd081
baptisteCable <baptiste.cable@gmail.com>
2013-11-27 10:24:47 +0100

    Edit group items.

--- src/SimpleIT/ClaireAppBundle/Services/Exercise/ExerciseModel/ExerciseModelService.php
+++ src/SimpleIT/ClaireAppBundle/Services/Exercise/ExerciseModel/ExerciseModelService.php
@@ -4,7 +4,11 @@ namespace SimpleIT\ClaireAppBundle\Services\Exercise\ExerciseModel;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use SimpleIT\ApiResourcesBundle\Exercise\ExerciseModel\Common\CommonModel;
+use SimpleIT\ApiResourcesBundle\Exercise\ExerciseModel\Common\ResourceBlock;
+use SimpleIT\ApiResourcesBundle\Exercise\ExerciseModel\GroupItems\ClassificationConstraints;
+use SimpleIT\ApiResourcesBundle\Exercise\ExerciseModel\GroupItems\Group;
 use SimpleIT\ApiResourcesBundle\Exercise\ExerciseModel\GroupItems\Model as GroupItems;
+use SimpleIT\ApiResourcesBundle\Exercise\ExerciseModel\GroupItems\ObjectBlock;
 use SimpleIT\ApiResourcesBundle\Exercise\ExerciseModel\MultipleChoice\Model as MultipleChoice;
 use SimpleIT\ApiResourcesBundle\Exercise\ExerciseModel\MultipleChoice\QuestionBlock;
 use SimpleIT\ApiResourcesBundle\Exercise\ExerciseModel\OrderItems\Model as OrderItems;
@@ -115,16 +119,55 @@ class ExerciseModelService
      */
     public function saveMultipleChoice($exerciseModelId, array $mcArray)
     {
-//        throw new \Exception(print_r($mcArray, true));
         $multipleChoice = new MultipleChoice();
         $this->setWordingAndDocuments($mcArray, $multipleChoice);
         $multipleChoice->setShuffleQuestionsOrder($mcArray['shuffle']);
-        $this->addQuestionBlocksFromArray($mcArray, $multipleChoice);
+        $this->addMultipleChoiceQuestionBlocksFromArray($mcArray, $multipleChoice);
 
         $exerciseModel = new ExerciseModelResource();
         $exerciseModel->setTitle($mcArray['title']);
         $exerciseModel->setType(CommonModel::MULTIPLE_CHOICE);
         $exerciseModel->setContent($multipleChoice);
+
+        return $this->save($exerciseModelId, $exerciseModel);
+    }
+
+    /**
+     * Save a group items
+     *
+     * @param int   $exerciseModelId
+     * @param array $giArray
+     *
+     * @throws \Exception
+     * @return ExerciseModelResource
+     */
+    public function saveGroupItems($exerciseModelId, array $giArray)
+    {
+//        throw new \Exception(print_r($giArray, true));
+        $groupItems = new GroupItems();
+        $this->setWordingAndDocuments($giArray, $groupItems);
+        switch ($giArray['displayGroupNames']) {
+            case "ask":
+            case "show":
+            case "hide":
+                $groupItems->setDisplayGroupNames($giArray['displayGroupNames']);
+                break;
+            default:
+                throw new \Exception('Invalid display group names value');
+        }
+
+        $localGroups = true;
+        if (isset($giArray['group']['root']) && !empty($giArray['group']['root'][0])) {
+            $localGroups = false;
+            $groupItems->setClassifConstr($this->createClassifConstraints($giArray, 'root'));
+        }
+
+        $this->addGroupItemsObjectBlocksFromArray($giArray, $groupItems, $localGroups);
+
+        $exerciseModel = new ExerciseModelResource();
+        $exerciseModel->setContent($groupItems);
+
+//        throw new \Exception(print_r($exerciseModel, true));
 
         return $this->save($exerciseModelId, $exerciseModel);
     }
@@ -150,43 +193,6 @@ class ExerciseModelService
             $exerciseModelId,
             new ArrayCollection($requiredResources)
         );
-    }
-
-    /**
-     * Add an exercise model
-     *
-     * @param ExerciseModelResource $exerciseModel
-     *
-     * @return ExerciseModelResource
-     */
-    public function addFromType(ExerciseModelResource $exerciseModel)
-    {
-        $content = null;
-        switch ($exerciseModel->getType()) {
-            case CommonModel::MULTIPLE_CHOICE:
-                $content = new MultipleChoice();
-                break;
-            case CommonModel::GROUP_ITEMS:
-                $content = new GroupItems();
-                break;
-            case CommonModel::ORDER_ITEMS:
-                $content = new OrderItems();
-                break;
-            case CommonModel::PAIR_ITEMS:
-                $content = new PairItems();
-                break;
-        }
-
-        $content->setWording("Consigne");
-
-        $exerciseModel->setContent($content);
-        $exerciseModel->setRequiredExerciseResources(array());
-        $exerciseModel->setTitle('Titre du modèle d\'exercices');
-        $exerciseModel = $this->add($exerciseModel);
-
-        $this->ownerExerciseModelService->addBasicFromExerciseModel($exerciseModel->getId());
-
-        return $exerciseModel;
     }
 
     /**
@@ -247,9 +253,11 @@ class ExerciseModelService
 
         $documents = array();
         foreach ($modelArray['documents'] as $docId) {
-            $document = new ModelDocument();
-            $document->setId($docId);
-            $documents[] = $document;
+            if (!empty($docId)) {
+                $document = new ModelDocument();
+                $document->setId($docId);
+                $documents[] = $document;
+            }
         }
         $model->setDocuments($documents);
     }
@@ -262,7 +270,7 @@ class ExerciseModelService
      *
      * @throws \Exception
      */
-    private function addQuestionBlocksFromArray(array $modelArray, &$model)
+    private function addMultipleChoiceQuestionBlocksFromArray(array $modelArray, &$model)
     {
         $questionBlocks = array();
 
@@ -273,73 +281,249 @@ class ExerciseModelService
                 $blockArray['maxNumberOfRightPropositions']
             );
 
-            if ($blockArray['resourceOrigin'] === "list") {
-                $resourceList = array();
-                foreach ($modelArray['resources'][$key] as $resId) {
-                    $resource = new ObjectId();
-                    $resource->setId($resId);
-                    $resourceList[] = $resource;
-                }
-                $block->setResources($resourceList);
-            } elseif ($blockArray['resourceOrigin'] === "constraints") {
-                $objConstraint = new ObjectConstraints();
-                $objConstraint->setType(CommonModel::MULTIPLE_CHOICE);
-
-                $mdConstraints = array();
-                foreach ($modelArray['key'][$key] as $constrKey => $metaKey) {
-                    $mdConstraint = new MetadataConstraint();
-                    $mdConstraint->setKey($metaKey);
-                    switch ($modelArray['comparator'][$key][$constrKey]) {
-                        case MetadataConstraint::IN:
-                            if (count($modelArray['values'][$key][$constrKey]) == 0) {
-                                throw new \Exception('Invalid value list');
-                            }
-                            $mdConstraint->setValueIn($modelArray['values'][$key][$constrKey]);
-                            break;
-                        case MetadataConstraint::BETWEEN:
-                            if (count($modelArray['values'][$key][$constrKey]) != 2) {
-                                throw new \Exception('Invalid value list');
-                            }
-                            $keys = array_keys($modelArray['values'][$key][$constrKey]);
-                            $mdConstraint->setBetween
-                                (
-                                    $modelArray['values'][$key][$constrKey][$keys[0]],
-                                    $modelArray['values'][$key][$constrKey][$keys[1]]
-                                );
-                            break;
-                        case MetadataConstraint::GREATER_THAN:
-                        case MetadataConstraint::GREATER_THAN_OR_EQUALS:
-                        case MetadataConstraint::LOWER_THAN:
-                        case MetadataConstraint::LOWER_THAN_OR_EQUALS:
-                            if (count($modelArray['values'][$key][$constrKey]) != 1) {
-                                throw new \Exception('Invalid value list');
-                            }
-                            $keys = array_keys($modelArray['values'][$key][$constrKey]);
-                            $mdConstraint->setComparison
-                                (
-                                    $modelArray['comparator'][$key][$constrKey],
-                                    $modelArray['values'][$key][$constrKey][$keys[0]]
-                                );
-                            break;
-                        case MetadataConstraint::EXISTS:
-                            if (count($modelArray['values'][$key][$constrKey]) > 0) {
-                                throw new \Exception('Invalid value list');
-                            }
-                            $mdConstraint->setExists();
-                            break;
-                    }
-                    $mdConstraints[] = $mdConstraint;
-                    $objConstraint->setMetadataConstraints($mdConstraints);
-                    $block->setResourceConstraint($objConstraint);
-                }
-                $objConstraint->setMetadataConstraints($mdConstraints);
-            } else {
-                throw new \Exception('Invalid request: multiple choice model creation');
-            }
+            $this->setResourceOrigin(
+                $block,
+                $blockArray,
+                $modelArray,
+                $key,
+                CommonModel::MULTIPLE_CHOICE
+            );
 
             $questionBlocks[] = $block;
         }
 
         $model->setQuestionBlocks($questionBlocks);
+    }
+
+    /**
+     * Create the object blocks from the modelArray
+     *
+     * @param array      $modelArray
+     * @param GroupItems $model
+     * @param boolean    $localGroups
+     */
+    private function addGroupItemsObjectBlocksFromArray(array $modelArray, &$model, $localGroups)
+    {
+        $objectBlocks = array();
+
+        foreach ($modelArray['blocks'] as $blockId => $blockArray) {
+            $block = new ObjectBlock($blockArray['numberOfOccurences']);
+
+            if ($localGroups) {
+                $block->setClassifConstr($this->createClassifConstraints($modelArray, $blockId));
+            }
+
+            $this->setResourceOrigin(
+                $block,
+                $blockArray,
+                $modelArray,
+                $blockId
+            );
+
+            $objectBlocks[] = $block;
+        }
+
+        $model->setObjectBlocks($objectBlocks);
+    }
+
+    /**
+     * Set the resource list or resource constraints in a resource block
+     *
+     * @param ResourceBlock $block
+     * @param array         $blockArray
+     * @param array         $modelArray
+     * @param int|string    $blockId
+     * @param null          $type
+     *
+     * @throws \Exception
+     */
+    private function setResourceOrigin(
+        ResourceBlock &$block,
+        array $blockArray,
+        array $modelArray,
+        $blockId,
+        $type = null
+    )
+    {
+        if ($blockArray['resourceOrigin'] === "list") {
+            $resourceList = array();
+            foreach ($modelArray['resources'][$blockId] as $resId) {
+                $resource = new ObjectId();
+                $resource->setId($resId);
+                $resourceList[] = $resource;
+            }
+            $block->setResources($resourceList);
+        } elseif ($blockArray['resourceOrigin'] === "constraints") {
+            $objConstraint = new ObjectConstraints();
+            if ($type === null) {
+                $type = $modelArray['type'][$blockId];
+            }
+            $objConstraint->setType($type);
+
+            $mdConstraints = array();
+            foreach ($modelArray['key'][$blockId] as $constrKey => $metaKey) {
+                $mdConstraints[] = $this->createMdConstraint(
+                    $metaKey,
+                    $modelArray['comparator'][$blockId][$constrKey],
+                    $modelArray['values'][$blockId][$constrKey]
+                );
+            }
+            $objConstraint->setMetadataConstraints($mdConstraints);
+            $block->setResourceConstraint($objConstraint);
+        } else {
+            throw new \Exception('Invalid request: resource origin');
+        }
+    }
+
+    /**
+     * Add an exercise model
+     *
+     * @param ExerciseModelResource $exerciseModel
+     *
+     * @return ExerciseModelResource
+     */
+    public function addFromType(ExerciseModelResource $exerciseModel)
+    {
+        $content = null;
+        switch ($exerciseModel->getType()) {
+            case CommonModel::MULTIPLE_CHOICE:
+                $content = new MultipleChoice();
+                break;
+            case CommonModel::GROUP_ITEMS:
+                $content = new GroupItems();
+                break;
+            case CommonModel::ORDER_ITEMS:
+                $content = new OrderItems();
+                break;
+            case CommonModel::PAIR_ITEMS:
+                $content = new PairItems();
+                break;
+        }
+
+        $content->setWording("Consigne");
+
+        $exerciseModel->setContent($content);
+        $exerciseModel->setRequiredExerciseResources(array());
+        $exerciseModel->setTitle('Titre du modèle d\'exercices');
+        $exerciseModel = $this->add($exerciseModel);
+
+        $this->ownerExerciseModelService->addBasicFromExerciseModel($exerciseModel->getId());
+
+        return $exerciseModel;
+    }
+
+    /**
+     * Create classification constraints from the arrays
+     *
+     * @param array $giArray
+     * @param       $blockId
+     *
+     * @return ClassificationConstraints
+     * @throws \Exception
+     */
+    private function createClassifConstraints(array $giArray, $blockId)
+    {
+        $classificationConstraints = new ClassificationConstraints();
+        switch ($giArray['other'][$blockId]) {
+            case 'own':
+            case 'reject':
+            case 'misc':
+                $classificationConstraints->setOther($giArray['other'][$blockId]);
+                break;
+            default:
+                throw new \Exception('Invalid other value');
+        }
+
+        $groups = array();
+        $metaKeys = array();
+        foreach ($giArray['group'][$blockId] as $groupId => $groupName) {
+            $group = new Group();
+            $group->setName($groupName);
+            $mdConstraints = array();
+            foreach ($giArray['classifKey'][$blockId][$groupId] as $constraintId => $key) {
+                if (isset($giArray['classifValues'][$blockId][$groupId][$constraintId])) {
+                    $classificationValues =
+                        $giArray['classifValues'][$blockId][$groupId][$constraintId];
+                } else {
+                    $classificationValues = array();
+                }
+
+                $mdConstraints[] = $this->createMdConstraint(
+                    $key,
+                    $giArray['classifComparator'][$blockId][$groupId][$constraintId],
+                    $classificationValues
+                );
+                $group->setMDConstraints($mdConstraints);
+
+                if (array_search($key, $metaKeys, true) === false) {
+                    $metaKeys[] = $key;
+                }
+            }
+            $groups[] = $group;
+        }
+
+        $classificationConstraints->setGroups($groups);
+        $classificationConstraints->setMetaKeys($metaKeys);
+
+        return $classificationConstraints;
+    }
+
+    /**
+     * Create a mdConstraint object from arrays
+     *
+     * @param $metaKey
+     * @param $comparator
+     * @param $values
+     *
+     * @return MetadataConstraint
+     * @throws \Exception
+     */
+    private function createMdConstraint($metaKey, $comparator, $values)
+    {
+        $mdConstraint = new MetadataConstraint();
+        $mdConstraint->setKey($metaKey);
+        switch ($comparator) {
+            case MetadataConstraint::IN:
+                if (count($values) == 0) {
+                    throw new \Exception('Invalid value list');
+                }
+                $mdConstraint->setValueIn($values);
+                break;
+            case MetadataConstraint::BETWEEN:
+                if (count($values) != 2) {
+                    throw new \Exception('Invalid value list');
+                }
+                $keys = array_keys($values);
+                $mdConstraint->setBetween
+                    (
+                        $values[$keys[0]],
+                        $values[$keys[1]]
+                    );
+                break;
+            case MetadataConstraint::GREATER_THAN:
+            case MetadataConstraint::GREATER_THAN_OR_EQUALS:
+            case MetadataConstraint::LOWER_THAN:
+            case MetadataConstraint::LOWER_THAN_OR_EQUALS:
+                if (count($values) != 1) {
+                    throw new \Exception('Invalid value list');
+                }
+                $keys = array_keys($values);
+                $mdConstraint->setComparison
+                    (
+                        $comparator,
+                        $values[$keys[0]]
+                    );
+                break;
+            case MetadataConstraint::EXISTS:
+                if (count($values) > 0) {
+                    throw new \Exception('Invalid value list');
+                }
+                $mdConstraint->setExists();
+                break;
+            default :
+                throw new \Exception('Invalid comparator');
+        }
+
+        return $mdConstraint;
     }
 }
