a59dee0a5d40d9be938bc98e208434fee61ff755
Romain Kuzniak <romain.kuzniak@simple-it.fr>
2013-12-26 12:21:40 +0100

    Fix make use case

--- src/SimpleIT/ClaireAppBundle/Controller/Course/Component/CourseContentController.php
+++ src/SimpleIT/ClaireAppBundle/Controller/Course/Component/CourseContentController.php
@@ -11,9 +11,9 @@ use SimpleIT\ClaireAppBundle\UseCases\Course\Content\DTO\GetDraftContentRequestD
 use SimpleIT\ClaireAppBundle\UseCases\Course\Content\DTO\GetPublishedContentRequestDTO;
 use SimpleIT\ClaireAppBundle\UseCases\Course\Content\DTO\GetWaitingForPublicationContentRequestDTO;
 use SimpleIT\ClaireAppBundle\UseCases\Course\Content\DTO\SaveContentRequestDTO;
+use SimpleIT\ClaireAppBundle\UseCases\Course\Course\DTO\GetDraftCourseRequestDTO;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use SimpleIT\AppBundle\Annotation\Cache;
 
 /**
  * Class CourseContentController
@@ -39,17 +39,24 @@ class CourseContentController extends AppController
 
         switch ($status) {
             case CourseResource::STATUS_WAITING_FOR_PUBLICATION:
-                $getContentResponse = $useCaseFactory->make('GetWaitingForPublicationContent')->execute(new GetWaitingForPublicationContentRequestDTO($courseIdentifier));
+                $getContentResponse = $useCaseFactory
+                    ->make('GetWaitingForPublicationContent')
+                    ->execute(new GetWaitingForPublicationContentRequestDTO($courseIdentifier));
                 break;
             case CourseResource::STATUS_DRAFT:
-                $getContentResponse = $useCaseFactory->make('GetDraftContent')->execute(new GetDraftContentRequestDTO($courseIdentifier));
+                $getContentResponse = $useCaseFactory
+                    ->make('GetDraftContent')
+                    ->execute(new GetDraftContentRequestDTO($courseIdentifier));
                 break;
             default :
-                $getContentResponse = $useCaseFactory->make('GetPublishedContent')->execute(new GetPublishedContentRequestDTO($courseIdentifier));
+                $getContentResponse = $useCaseFactory
+                    ->make('GetPublishedContent')
+                    ->execute(new GetPublishedContentRequestDTO($courseIdentifier));
                 break;
         }
 
         /** @var GetContentResponse $getContentResponse */
+
         return $this->render(
             'SimpleITClaireAppBundle:Course/Course/Component:viewContent.html.twig',
             array('content' => $getContentResponse->getContent())
@@ -88,15 +95,15 @@ class CourseContentController extends AppController
      */
     public function editViewAction(Request $request, $courseId)
     {
-        $courseContent = $this->get('simple_it.claire.course.course')->getContentToEdit(
-            $courseId,
-            $request->get(CourseResource::STATUS, CourseResource::STATUS_DRAFT)
-        );
+        /** @var GetContentResponse $ucResponse */
+        $ucResponse = $this->get('simple_it.claire.use_cases.use_case_factory')
+            ->make('GetDraftContent')
+            ->execute(new GetDraftCourseRequestDTO($courseId));
 
         return $this->render(
             'SimpleITClaireAppBundle:Course/Course/Component:editContent.html.twig',
             array(
-                'content' => $courseContent,
+                'content' => $ucResponse->getContent(),
                 'action'  =>
                     $this->generateUrl(
                         'simple_it_claire_component_course_course_content_edit',
@@ -116,13 +123,10 @@ class CourseContentController extends AppController
      */
     public function editAction(Request $request, $courseId)
     {
-        $ucRequest = new SaveContentRequestDTO($courseId, $content = $request->get('content'));
-
-        /** @var UseCaseFactory $useCaseFactory */
-        $useCaseFactory = $this->get('simple_it.claire.use_cases.use_case_factory');
-
         /** @var SaveContentResponse $ucResponse */
-        $ucResponse = $useCaseFactory->make('SaveContent')->execute($ucRequest);
+        $ucResponse = $this->get('simple_it.claire.use_cases.use_case_factory')
+            ->make('SaveContent')
+            ->execute(new SaveContentRequestDTO($courseId, $content = $request->get('content')));
 
         return new Response($ucResponse->getContent());
     }
