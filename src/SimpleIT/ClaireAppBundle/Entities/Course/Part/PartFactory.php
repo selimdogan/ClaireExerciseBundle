912f74e998bcb931d738f9f7394b86d145e20d8e
Romain Kuzniak <romain.kuzniak@simple-it.fr>
2013-12-27 08:36:22 +0100

    Fix cs and tests

--- src/SimpleIT/ClaireAppBundle/Entities/Course/Part/PartFactory.php
+++ src/SimpleIT/ClaireAppBundle/Entities/Course/Part/PartFactory.php
@@ -2,21 +2,15 @@
 
 namespace SimpleIT\ClaireAppBundle\Entities\Course\Part;
 
+use SimpleIT\ApiResourcesBundle\Course\PartResource;
+
 /**
- * Interface PartFactory
- *
  * @author Romain Kuzniak <romain.kuzniak@simple-it.fr>
  */
 interface PartFactory
 {
-    const TITLE_1 = 'title-1';
-
-    const TITLE_2 = 'title-2';
-
-    const TITLE_3 = 'title-3';
-
     /**
-     * @return Part
+     * @return PartResource
      */
     public function make($subtype);
 }
--- src/SimpleIT/ClaireAppBundle/Entity/Course/Part/PartFactoryImpl.php
+++ src/SimpleIT/ClaireAppBundle/Entity/Course/Part/PartFactoryImpl.php
@@ -6,8 +6,6 @@ use SimpleIT\ApiResourcesBundle\Course\PartResource;
 use SimpleIT\ClaireAppBundle\Entities\Course\Part\PartFactory;
 
 /**
- * Class PartFactoryImpl
- *
  * @author Romain Kuzniak <romain.kuzniak@simple-it.fr>
  */
 class PartFactoryImpl implements PartFactory
@@ -18,17 +16,17 @@ class PartFactoryImpl implements PartFactory
     public function make($subtype)
     {
         switch ($subtype) {
-            case self::TITLE_1:
+            case PartResource::TITLE_1:
                 $part = new PartResource();
-                $part->setSubtype(self::TITLE_1);
+                $part->setSubtype(PartResource::TITLE_1);
                 break;
-            case self::TITLE_2:
+            case PartResource::TITLE_2:
                 $part = new PartResource();
-                $part->setSubtype(self::TITLE_2);
+                $part->setSubtype(PartResource::TITLE_2);
                 break;
-            case self::TITLE_3:
+            case PartResource::TITLE_3:
                 $part = new PartResource();
-                $part->setSubtype(self::TITLE_3);
+                $part->setSubtype(PartResource::TITLE_3);
                 break;
             default:
                 throw new \InvalidArgumentException();
