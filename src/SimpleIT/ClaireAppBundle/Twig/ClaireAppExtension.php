774cbdce3fd1164a04e5e21b37b3b47181a148e6
Gabriel Vieira <gabriel.viera@simple-it.fr>
2014-01-07 16:55:28 +0100

    Add CC_ND license

--- src/SimpleIT/ClaireAppBundle/Twig/ClaireAppExtension.php
+++ src/SimpleIT/ClaireAppBundle/Twig/ClaireAppExtension.php
@@ -77,6 +77,7 @@ class ClaireAppExtension extends \Twig_Extension
         if (!is_null($license)) {
             $content = '<i class="icon-cc" title="CC"></i>';
             $licenseArray = explode('-', $license);
+
             foreach ($licenseArray as $partLicense) {
                 switch ($partLicense) {
                     case MetadataResource::LICENSE_CC_BY:
@@ -87,6 +88,9 @@ class ClaireAppExtension extends \Twig_Extension
                         break;
                     case MetadataResource::LICENSE_CC_SA:
                         $content .= '<i class="icon-ccsa" title="SA"></i>';
+                        break;
+                    case MetadataResource::LICENSE_CC_ND:
+                        $content .= '<i class="icon-ccnd" title="ND"></i>';
                         break;
                     default:
                         break;
