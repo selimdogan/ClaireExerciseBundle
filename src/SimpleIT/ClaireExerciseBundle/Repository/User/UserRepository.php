89a0c256b170f0d4c04d76db47c574d4e3fec11f
baptisteCable <baptiste.cable@gmail.com>
2014-04-30 10:16:11 +0200

    Authentification

--- src/SimpleIT/ClaireExerciseBundle/Repository/User/UserRepository.php
+++ src/SimpleIT/ClaireExerciseBundle/Repository/User/UserRepository.php
@@ -2,14 +2,66 @@
 
 namespace SimpleIT\ClaireExerciseBundle\Repository\User;
 
+use Doctrine\ORM\NoResultException;
 use Doctrine\ORM\QueryBuilder;
 use SimpleIT\CoreBundle\Repository\BaseRepository;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 
 /**
  * Class UserRepository
  *
  * @author Baptiste Cablé <baptiste.cable@liris.cnrs.fr>
  */
-class UserRepository extends BaseRepository
+class UserRepository extends BaseRepository implements UserProviderInterface
 {
+    /**
+     * @inheritDoc
+     */
+    public function loadUserByUsername($username)
+    {
+        $q = $this
+            ->createQueryBuilder('u')
+            ->where('u.username = :username')
+            ->setParameter('username', $username)
+            ->getQuery();
+
+        try {
+            // La méthode Query::getSingleResult() lance une exception
+            // s'il n'y a pas d'entrée correspondante aux critères
+            $user = $q->getSingleResult();
+        } catch (NoResultException $e) {
+            throw new UsernameNotFoundException(sprintf('Unable to find an active admin AcmeUserBundle:User object identified by "%s".', $username), 0, $e);
+        }
+
+        return $user;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function refreshUser(UserInterface $user)
+    {
+        $class = get_class($user);
+        if (!$this->supportsClass($class)) {
+            throw new UnsupportedUserException(
+                sprintf(
+                    'Instances of "%s" are not supported.',
+                    $class
+                )
+            );
+        }
+
+        return $this->find($user->getId());
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function supportsClass($class)
+    {
+        return $this->getEntityName() === $class || is_subclass_of($class, $this->getEntityName());
+    }
 }
